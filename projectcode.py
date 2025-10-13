bd019c94da9b7c993f471e8a321d9fb1498a217b
Armana Akhsambiyeva <armanaakhsambiyeva@Armanas-MacBook-Pro-580.local>
2025-10-12 22:28:09 -0400

    second commit, first function opening csv file and list of dicts

--- projectcode.py
+++ projectcode.py
@@ -1,1 +1,22 @@
-#Armana Akhsambiyeva 
+# Name: Armana Akhsambiyeva 
+# Student ID: 1442 9696
+# uniqname: akhsambi 
+# email: akhsambi@umich.edu
+# Title of project: Calculating the Center and Variability of Penguin Data Set Measurements 
+
+import csv 
+import math 
+#Function 1: Opening CSV File 
+
+def read_penguin_data(filename):
+    with open(filename, mode='r', newline='' ) as f:
+        list_dicts = [] 
+        csvReader = csv.reader(f)
+        next(csvReader)
+        for row in csvReader: 
+            list_dicts.append(row)
+    return list_dicts
+
+
+
+
